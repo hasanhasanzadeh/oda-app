9ec075b02f6fc0431a74140a8b9e411e8e5962cb
hasanhasanzadeh <hasan.hasanzadeh.dev@gmail.com>
2025-10-12 15:56:27 +0330

    add tiems

---
+++ config/expired.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    // time expired at activation code by minute
+    'time_expired_at_activation_code'            => env('TIME_EXPIRED_AT_ACTIVATION_CODE', 2),
+    'digits_count' => env('DIGITS_COUNT', 5),
+];
